32324b988d370e477446c0fe72dd302359c2b325
aimohapatra <aishanim@Aishanis-MacBook-Pro.local>
2022-06-22 21:33:10 -0700

    organized functions

--- src/Game.java
+++ src/Game.java
@@ -4,11 +4,18 @@ import java.util.*;
 
 public class Game {
     public static void main(String[] args) {
-        System.out.println("your mom");
         File text = new File("/Users/aishanim/Desktop/Game-of-Life/small.txt");
+        char[][] board = createBoard(text);
+        //printBoard(board);
+        char[][] board2 = createBoard2(board);
+        nextIteration(board, board2, board.length, board[0].length);
+
+    }
+
+    public static char[][] createBoard(File fp) {
 
         try {
-            Scanner sc = new Scanner(text);
+            Scanner sc = new Scanner(fp);
             int rows = Integer.parseInt(sc.nextLine());
             int cols = Integer.parseInt(sc.nextLine());
             char[][] board = new char[rows][cols];
@@ -22,24 +29,51 @@ public class Game {
                 int currRow = Integer.parseInt(curr.split(" ")[0]) - 1;
                 int currCol = Integer.parseInt(curr.split(" ")[1]) - 1;
                 board[currRow][currCol] = 'X';
-                System.out.println(currRow + " " + currCol);
             }
             sc.close();
-
-            for(char[] i : board) {
-                for(char j : i) {
-                    System.out.print(j + " ");
-                }
-                System.out.println(" ");
-            }
+            return board;
         }
 
 
         catch(FileNotFoundException e) {
             e.printStackTrace();
+            return null;
         }
 
     }
 
+    public static char[][] createBoard2(char[][] input) {
+        char[][] newBoard = new char[input.length][input[0].length];
+        for(char[] i : newBoard) {
+            Arrays.fill(i, '-');
+        }
+        return newBoard;
+    }
 
+    public static void printBoard(char[][] board) {
+        for(char[] i : board) {
+            for(char j : i) {
+                System.out.print(j + " ");
+            }
+            System.out.println(" ");
+        }
+
+    }
+
+    public static char[][] nextIteration(char[][] board, char[][] board2, int rows, int cols) {
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                doCell(board, board2, i, j);
+            }
+        }
+        return null;
+    }
+
+    public static void doCell(char[][] board, char[][] board2, int rowIndex, int colIndex) {
+        System.out.print(rowIndex + " " + colIndex + "\n");
+
+        board2[rowIndex][colIndex] = '-'; //if dead in next iteration
+        board2[rowIndex][colIndex] = 'X'; //if alive in next iteration
+
+    }
 }
